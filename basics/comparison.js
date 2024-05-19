538a61831675f5e95747f8d094d3dde388aeaf5d
Muppukokila <127586949+Muppukokila@users.noreply.github.com>
2024-05-19 06:34:41 +0000

    completed comparisons

---
+++ basics/comparison.js
@@ -0,0 +1,36 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2!=1);
+
+
+//*******don't do this type of comparisons***** */
+//console.log(null>0); //false because 0>0 false
+
+//console.log(null==0);// treats as different values so false
+
+//console.log(null>=0);// true because 0>=0 true
+
+
+//here equality == & comparisons ><>=<= work differently comparison treats null as 0, so >= so its is true
+
+// console.log(undefined==0); //false
+// console.log(undefined>0);  //false
+// console.log(undefined<0);  //false
+/**********don't do this type of comparisons ****/
+
+
+//strict check ===
+
+console.log("2"==2);//true not checking type
+console.log("2"===2);//false checking type
+
+
+
+
+
+
+
+
