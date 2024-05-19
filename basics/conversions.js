18aeb3c7ba38e5c1e1ec937be0b9daf5ab522715
Muppukokila <127586949+Muppukokila@users.noreply.github.com>
2024-05-19 05:32:00 +0000

    conversions completed

---
+++ basics/conversions.js
@@ -0,0 +1,25 @@
+/*let score=33;*/ /*let score="33abc"*/ let score=null;
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);  //return NaN =>not a number for null =>zero & null is object for undefined=> NaN
+
+//"33"=>33
+//"33abc" => NaN
+//true =>1; false=>0
+//undefined => NaN
+
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//"" =>false
+//"kokila" =>true
+
+let someNumber = 33;
+let stringToNumber=String(someNumber);
+console.log(stringToNumber);
+console.log(typeof(stringToNumber));
