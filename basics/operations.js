fa23acfacc3b2ae9c54453a76925cb28db0dcba2
Muppukokila <127586949+Muppukokila@users.noreply.github.com>
2024-05-19 05:59:07 +0000

    string operations completed

---
+++ basics/operations.js
@@ -0,0 +1,39 @@
+// let someNumber=33;
+
+// let NumberToString=String(someNumber);
+// console.log(NumberToString);
+// console.log(typeof NumberToString);
+
+//******************operations *******************
+
+// let value=3;
+// let negValue=-value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="hello";
+let str2="kokila";
+
+//let str3=str1+str2;
+console.log(str1+" "+str2);
+//wrong way of writing codes but used for understanding purpose
+// console.log("1"+2); //12
+// console.log(1+"2"); //12
+// console.log("1"+2+2);//122 not added 2 &2
+// console.log(1+2+"2");//32  added 1 & 2
+// console.log(+true);//1
+// //console.log(true+); //error
+// console.log(+""); //0
+
+let gameCounter=100;
+//gameCounter++;
+++gameCounter;//output is same but both r not same
+console.log(gameCounter);
+
+
