3702aef9550fb314107148ba71647f71feebe304
Muppu Kokila <127586949+Muppukokila@users.noreply.github.com>
2024-05-26 08:46:31 +0000

    data_types summary completed

---
+++ basics/datatypes_summary.js
@@ -0,0 +1,31 @@
+// #Primitive 
+
+//  7 types :String,Number,Boolearn,null,undefined,Symbol,BigInt
+
+// dynically language
+// typescript 
+// const score:number =100; type safety
+
+// const score=100 number
+// const scoreValue=100.3; number
+// const isLoggedIn=false; boolean
+// const outsideTemp=null; null
+// const userEmail;  undefined
+const id= Symbol('123');
+const anotherId=Symbol('123');
+// console.log(id===anotherId);  false not same
+// const bigNumber=347657589n;
+
+
+// non Primitive (or)Reference
+
+// Array,Objects,Functions
+const heros=["shaktiman","naagraj","doga"]
+const myObj={
+    name:"kokila",
+    age:21,
+};
+const myFunction=function(){
+    console.log("hello world");
+}
+
