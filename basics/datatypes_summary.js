1962746e091576d4044c1a4c96dc773fcd18bcd3
Muppu Kokila <127586949+Muppukokila@users.noreply.github.com>
2024-05-26 08:46:52 +0000

    string added

--- basics/datatypes_summary.js
+++ basics/datatypes_summary.js
@@ -16,7 +16,6 @@ const anotherId=Symbol('123');
 // console.log(id===anotherId);  false not same
 // const bigNumber=347657589n;
 
-
 // non Primitive (or)Reference
 
 // Array,Objects,Functions
@@ -29,3 +28,23 @@ const myFunction=function(){
     console.log("hello world");
 }
 
+//https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// ++++++++++++++++++++=++++++++++++
+// Stack(Primitive),Heap(Non-Primitive)
+
+let myName="kokila";
+let anotherName=myName;
+anotherName="muppu";   //store as stack
+console.log(myName);
+console.log(anotherName);
+
+let userOne={
+    email:"user@google.com",
+    upi:"user@ybt"
+};
+
+let userTwo=userOne;
+userTwo.email="kokila@google.com";
+console.log(userOne);
+console.log(userTwo);
